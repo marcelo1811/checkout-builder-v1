4bdea1af167c3de15974341cc27bd9f4870d3485
Marcelo Miyachi <marcelo.m1811@gmail.com>
2021-10-23 17:35:08 -0300

    feat: add CardBlock

--- src/blocks/CardBlock.tsx
+++ src/blocks/CardBlock.tsx
@@ -3,12 +3,31 @@ import { Box, Text } from "@chakra-ui/layout";
 interface ICardBlockProps {
   title: string;
   description?: string;
+  bgColor?: string;
+  containerStyles?: any;
+  fontColor?: string;
 }
 
-export function CardBlock({ title, description }: ICardBlockProps) {
+export function CardBlock({ title, description, bgColor, containerStyles, fontColor }: ICardBlockProps) {
   return (
-    <Box>
-      <Text>{title}</Text>
+    <Box
+      bg={bgColor}
+      display='flex'
+      flexDirection='column'
+      padding='15px'
+      style={containerStyles}
+      borderRadius='5px'
+      justifyContent='center'
+      alignItems='center'
+      minWidth='250px'
+      maxWidth='360px'
+      width='280px'
+    >
+      <Text
+        color={fontColor}
+        fontSize='18px'
+        fontWeight='bold'
+      >{title}</Text>
       {!!description && <Text>{description}</Text>}
     </Box>
   )
--- src/stories/CardBlock.stories.tsx
+++ src/stories/CardBlock.stories.tsx
@@ -13,4 +13,6 @@ export const Basic = Template.bind({});
 Basic.args = {
   title: 'Aprenda na prática',
   description: 'Exemplos da vida real e código',
+  bgColor: '#eae8e8',
+  fontColor: 'rgb(3, 224, 2)',
 }
