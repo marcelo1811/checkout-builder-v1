ec4bbf068e30811acb99ff8cddaae2e5fe3e89cc
Marcelo Miyachi <marcelo.m1811@gmail.com>
2021-10-23 17:21:43 -0300

    feat: add storybook

---
+++ src/stories/CardBlock.stories.tsx
@@ -0,0 +1,16 @@
+import { ComponentStory } from "@storybook/react";
+import { CardBlock } from "blocks/CardBlock";
+
+export default {
+  title: 'Example/CardBlock',
+  component: CardBlock,
+}
+
+const Template: ComponentStory<typeof CardBlock> = (args) => <CardBlock {...args} />;
+
+export const Basic = Template.bind({});
+
+Basic.args = {
+  title: 'Aprenda na prática',
+  description: 'Exemplos da vida real e código',
+}
