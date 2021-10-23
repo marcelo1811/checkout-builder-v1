5cd3ecf57c26bc615b6de4fb741ae8c4833cd63d
Marcelo Miyachi <marcelo.m1811@gmail.com>
2021-10-23 16:48:20 -0300

    feat: add chakra ui

---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,14 @@
+import { Box, Text } from "@chakra-ui/layout";
+import { Container } from "@chakra-ui/react";
+
+export function HomePage() {
+  return (
+    <Box
+      bg='red'
+      height='100vh'
+      width='100vw'
+    >
+      <Text>HomePage</Text>
+    </Box>
+  )
+}
